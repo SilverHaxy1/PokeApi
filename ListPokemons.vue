dbb79bad8c795c4ee8e70efd9e0bf9d5ae485b5b
SilverHaxy1 <112134456+SilverHaxy1@users.noreply.github.com>
2023-09-26 19:36:51 -0300

    Add files via upload

---
+++ ListPokemons.vue
@@ -0,0 +1,34 @@
+<script setup>
+const pokemon = defineProps(["name","baseUrlSvg"])
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card p-2 mb-3">
+            <p class="text-center">{{ pokemon.name }}</p>
+            <img
+            :src="baseUrlSvg" class="card-img-top"
+            alt="..."
+            height="80"
+                />
+        </div>
+    </div>
+     
+
+</template>
+
+<style>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    width: calc(100% - 20px);
+    min-height: calc(100vh - 20px);
+    
+    font-family: 'acme', arial;
+    font-size: 1rem;
+}
+
+</style>
---
+++ PokemonCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+const pokemon = defineProps(["Name","xp","height","img"])
+</script>
+
+
+
+<template>
+    <div class="card">
+            <img 
+            :src="pokemon.img" class="card-img-top" 
+            :alt="pokemon.name">
+            <div class="card-body">
+                <h5 class="card-title text-center">{{pokemon.name}}</h5>
+                
+                <hr> 
+                <div class="row">
+                    <section class="col">
+                        <strong>XP:</strong>
+                        <span>{{pokemon.xp}}</span>
+                    </section>
+                    <section class="col">
+                        <strong>height:</strong>
+                        <span>{{pokemon.height}}</span>
+                    </section>
+                </div>
+            </div>
+        </div>
+</template>
+
+
+
+<style>
+
+</style>
